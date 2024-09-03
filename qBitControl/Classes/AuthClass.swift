74bda78744814f769ab21375002353055c0f1591
Michael-128 <michal290704@gmail.com>
2024-09-03 10:37:15 +0200

    Fix Password Encoding - Users can now use passwords with any special characters

--- qBitControl/Classes/AuthClass.swift
+++ qBitControl/Classes/AuthClass.swift
@@ -17,52 +17,48 @@ class Auth {
     }
     
     static func getCookie(url: String, username: String, password: String, isSuccess: @escaping (Bool) -> Void, setCookie: Bool = true) async {
-        let urlString = url;
-        guard let url = URL(string: "\(url)/api/v2/auth/login") else {return}
-        
+        let urlString = url
+        guard let url = URL(string: "\(url)/api/v2/auth/login") else { return }
+
         var req = URLRequest(url: url)
         req.httpMethod = "POST"
 
-        var urlComponents = URLComponents()
-        urlComponents.queryItems = [
-            URLQueryItem(name: "username", value: "\(username)"),
-            URLQueryItem(name: "password", value: "\(password)")
+        // Create a properly encoded query string
+        let parameters: [String: String] = [
+          "username": username,
+          "password": password
         ]
         
-        let bodyString = urlComponents.string
-        guard let bodyString = bodyString?.suffix((bodyString?.count ?? 1) - 1) else { return }
-        let data = bodyString.data(using: .utf8)
-        req.httpBody = data
-        
+        let parameterArray = parameters.map { key, value in
+           return "\(key)=\(value.addingPercentEncoding(withAllowedCharacters: .alphanumerics) ?? "")"
+        }
+        let bodyString = parameterArray.joined(separator: "&")
+        print(bodyString)
+        req.httpBody = bodyString.data(using: .utf8)
+
         let sessionConfiguration = URLSessionConfiguration.default
-        
         sessionConfiguration.timeoutIntervalForRequest = 10
-        
         let session = URLSession(configuration: sessionConfiguration)
-        
+
         await session.reset()
-        
-        session.dataTask(with: req) {
-            data, response, error in
-            /*if let data = data {
-                print(data)
-            }*/
-            if let response = response as? HTTPURLResponse {
-                let cookie = String(String(describing: response.allHeaderFields["Set-Cookie"] ?? "n/a;").split(separator: ";")[0])
-                if(cookie.contains("SID")) {
-                    if(setCookie) {
-                        qBittorrent.setURL(url: urlString)
-                        qBittorrent.setCookie(cookie: cookie)
-                    }
-                    isSuccess(true)
-                } else {
-                    isSuccess(false)
-                }
-            }
-            if let error = error {
-                print(error.localizedDescription)
-                isSuccess(false)
-            }
+
+        session.dataTask(with: req) { data, response, error in
+          if let response = response as? HTTPURLResponse {
+              let cookie = String(String(describing: response.allHeaderFields["Set-Cookie"] ?? "n/a;").split(separator: ";")[0])
+              if cookie.contains("SID") {
+                  if setCookie {
+                      qBittorrent.setURL(url: urlString)
+                      qBittorrent.setCookie(cookie: cookie)
+                  }
+                  isSuccess(true)
+              } else {
+                  isSuccess(false)
+              }
+          }
+          if let error = error {
+              print(error.localizedDescription)
+              isSuccess(false)
+          }
         }.resume()
     }
 }
