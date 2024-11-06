a0c807090da6b728f89ca18cfcf09b0251169730
Michael-128 <michal290704@gmail.com>
2024-11-06 21:10:08 +0100

    Fix Article Filtering

--- qBitControl/Views/RSSViews/RSSFeedView.swift
+++ qBitControl/Views/RSSViews/RSSFeedView.swift
@@ -5,15 +5,15 @@ struct RSSFeedView: View {
     @State public var searchQuery: String = ""
     
     func filterArticles(_ article: RSSFeed.Article) -> Bool {
-        if let title = article.title { return title.contains(searchQuery) }
+        if let title = article.title { return title.lowercased().contains(searchQuery.lowercased()) }
         return false
     }
     
     var body: some View {
         List {
             if !searchQuery.isEmpty {
-                Section(header: Text("\(rssFeed.articles.count { filterArticles($0) }) Articles") ) {
-                    ForEach(rssFeed.articles.filter { filterArticles($0) }, id: \.id) { article in
+                Section(header: Text("\(rssFeed.articles.count) Articles") ) {
+                    ForEach(rssFeed.articles.filter(filterArticles), id: \.id) { article in
                         RSSArticleView(article: article)
                     }
                 }
