6dbb351ccc881d5e3d8760963896c214e7bc3b37
Michael-128 <michal290704@gmail.com>
2025-04-16 21:39:56 +0200

    Add no results message & increase build number

--- qBitControl/Views/SearchViews/SearchView.swift
+++ qBitControl/Views/SearchViews/SearchView.swift
@@ -4,33 +4,41 @@ struct SearchView: View {
     @StateObject var viewModel = SearchViewModel()
     
     var body: some View {
-        List {
-            Section(header: Text("Search")) {
-                HStack {
-                    TextField("Search", text: $viewModel.query)
-                        .autocorrectionDisabled(true)
-                        .autocapitalization(.none)
-                        .keyboardType(.default)
-
-                    Button {
-                        viewModel.startSearch()
-                    } label: {
-                        if viewModel.isRunning {
-                            Text("Running...")
-                                .foregroundStyle(.gray)
-                        } else {
-                            Text("Start")
+        ZStack {
+            List {
+                Section(header: Text("Search")) {
+                    HStack {
+                        TextField("Search", text: $viewModel.query)
+                            .autocorrectionDisabled(true)
+                            .autocapitalization(.none)
+                            .keyboardType(.default)
+                        
+                        Button {
+                            viewModel.startSearch()
+                        } label: {
+                            if viewModel.isRunning {
+                                Text("Running...")
+                                    .foregroundStyle(.gray)
+                            } else {
+                                Text("Start")
+                            }
+                        }
+                    }
+                }
+                
+                if viewModel.isResponse {
+                    Section(header: Text("\(viewModel.lastestTotal)" + " " + "results")) {
+                        ForEach(viewModel.latestResults, id: \.hashValue) { result in
+                            SearchRowView(result: result)
                         }
                     }
                 }
             }
             
-            if viewModel.isResponse {
-                Section(header: Text("\(viewModel.lastestTotal)" + " " + "results")) {
-                    ForEach(viewModel.latestResults, id: \.hashValue) { result in
-                        SearchRowView(result: result)
-                    }
-                }
+            if !viewModel.isResponse {
+                VStack {
+                    Text("No results")
+                }.foregroundStyle(.gray)
             }
         }
     }
